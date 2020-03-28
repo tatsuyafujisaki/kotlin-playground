3500828cfa42df7b6325a988a1a86a876a50cc81
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-03-28 13:59:24 +0900

    Create HandmadeSortingAlgorithms.kt

---
+++ src/main/kotlin/samples/HandmadeSortingAlgorithms.kt
@@ -0,0 +1,23 @@
+package samples
+
+object HandmadeSortingAlgorithms {
+    fun bubbleSort(xs: IntArray) {
+        var n = xs.size
+        do {
+            var lastSwappedIndex = 0
+            for(i in 0..(n - 2)) {
+                if(xs[i + 1] < xs[i]) {
+                    swap(xs, i, i + 1)
+                    lastSwappedIndex = i + 1
+                }
+            }
+            n = lastSwappedIndex
+        } while (1 < n)
+    }
+
+    private fun swap(xs: IntArray, i: Int, j: Int) {
+        val tmp = xs[i]
+        xs[i] = xs[j]
+        xs[j] = tmp
+    }
+}

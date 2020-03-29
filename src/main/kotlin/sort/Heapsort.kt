452b874ba3b1a83cf48e6a31f46755398f2d42d5
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-03-29 16:00:24 +0900

    Update Heapsort.kt

--- src/main/kotlin/sort/Heapsort.kt
+++ src/main/kotlin/sort/Heapsort.kt
@@ -4,29 +4,36 @@ import utils.swap
 
 object Heapsort {
     fun sort(xs: IntArray) {
+        // Build a binary max heap. i.e. Create a binary max heap except the deepest nodes.
         for (i in xs.size / 2 downTo 0) {
             heapify(xs, xs.size, i)
         }
 
-        for (i in xs.indices.last downTo 0) {
-            swap(xs, 0, i)
-            heapify(xs, i, 0)
+        for (lastIndex in xs.indices.last downTo 0) {
+            // Move the largest node, which is at the first element of the array, to the end of the array.
+            swap(xs, 0, lastIndex)
+
+            // Create a binary max heap using all the nodes before the last index.
+            heapify(xs, lastIndex, 0)
         }
     }
 
-    private fun heapify(xs: IntArray, n: Int, currentNodexIndex: Int) {
-        var indexOfMax = currentNodexIndex
-        val leftChildIndex = 2 * currentNodexIndex + 1
-        val rightChildIndex = 2 * currentNodexIndex + 2
-        if (leftChildIndex < n && xs[indexOfMax] < xs[leftChildIndex]) {
+    /**
+     * Create a binary max heap.
+     */
+    private fun heapify(xs: IntArray, nodeCount: Int, currentNodeIndex: Int) {
+        var indexOfMax = currentNodeIndex
+        val leftChildIndex = 2 * currentNodeIndex + 1
+        val rightChildIndex = 2 * currentNodeIndex + 2
+        if (leftChildIndex < nodeCount && xs[indexOfMax] < xs[leftChildIndex]) {
             indexOfMax = leftChildIndex
         }
-        if (rightChildIndex < n && xs[indexOfMax] < xs[rightChildIndex]) {
+        if (rightChildIndex < nodeCount && xs[indexOfMax] < xs[rightChildIndex]) {
             indexOfMax = rightChildIndex
         }
-        if (indexOfMax != currentNodexIndex) {
-            swap(xs, currentNodexIndex, indexOfMax)
-            heapify(xs, n, indexOfMax)
+        if (currentNodeIndex != indexOfMax) {
+            swap(xs, currentNodeIndex, indexOfMax)
+            heapify(xs, nodeCount, indexOfMax)
         }
     }
 }

104ac82cb59e9c1706ce4f2cf8de38dd105a6ce6
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-04-20 21:51:25 +0900

    Update GsonJsonConverter.kt

--- src/main/kotlin/converters/GsonJsonConverter.kt
+++ src/main/kotlin/converters/GsonJsonConverter.kt
@@ -5,5 +5,5 @@ import com.google.gson.Gson
 object GsonJsonConverter {
     val gson = Gson()
     fun <T> toJson(src: T): String = gson.toJson(src)
-    inline fun <reified T> fromJson(src: String): T = gson.fromJson(src, T::class.java)
+    inline fun <reified T> fromJson(json: String): T? = gson.fromJson(json, object : TypeToken<T>() {}.type)
 }

f1d1235425960866aa9fbe1ae12596b334ff29ab
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-06-05 00:59:17 +0900

    Update StringUtil.kt

--- src/main/kotlin/util/StringUtil.kt
+++ src/main/kotlin/util/StringUtil.kt
@@ -3,20 +3,19 @@ package util
 object StringUtil {
     /**
      * groupValues returns a list of two elements.
-     * Its first element is the entire match including a trailing double quotation mark, which is not what we want.
-     * Its second element is the first match.
-     *
-     * Usage:
-     * val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
-     * val url = extractDoubleQuotedUrl(html) // https://example.com
+     * The first element is the entire match including a trailing double quotation mark we don't want.
+     * The second element is the first match, which is what we want.
      */
-    fun extractDoubleQuotedUrl(s: String) = Regex("(https?://.*?)\"").find(s)?.groupValues?.last()
+    fun extractDoubleQuotedUrl(s: String) = Regex("(https?://.+?)\"").find(s)?.groupValues?.last()
 
-    /**
-     * Usage:
-     * val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
-     * val url = extractDoubleQuotedUrl(html) // https://example.com
-     */
     fun extractDoubleQuotedUrl2(s: String) =
-        Regex("(https?://.*?)\"").find(s)?.destructured?.component1()
+        Regex("(https?://.+?)\"").find(s)?.destructured?.component1()
+
+    fun sampleUsage() {
+        val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
+        val url = extractDoubleQuotedUrl(html) // https://example.com
+        val url2 = extractDoubleQuotedUrl2(html) // https://example.com
+        println(url)
+        println(url2)
+    }
 }

6af1180eae21d2e38b8f8cbd9a495493d114cc49
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-07-05 03:35:29 +0900

    Add String.equalsIgnoreCase()

--- src/main/kotlin/util/StringUtil.kt
+++ src/main/kotlin/util/StringUtil.kt
@@ -3,6 +3,7 @@ package util
 object StringUtil {
     fun String?.isNeitherNullNorEmpty(): Boolean = this != null && isNotEmpty()
     fun String?.isNeitherNullNorBlank(): Boolean = this != null && isNotBlank()
+    fun String.equalsIgnoreCase(s2: String) = equals(s2, true)
 
     /**
      * groupValues returns a list of two elements.

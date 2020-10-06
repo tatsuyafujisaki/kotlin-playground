7578bc23d440074a58b3a8d3a4467ad86e0f85c6
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-10-06 22:35:50 +0900

    Add example to SubjectExamples

--- src/main/kotlin/rx/SubjectExamples.kt
+++ src/main/kotlin/rx/SubjectExamples.kt
@@ -5,7 +5,43 @@ import io.reactivex.rxjava3.subjects.PublishSubject
 import io.reactivex.rxjava3.subjects.Subject
 
 object SubjectExamples {
+    var firstExecution = true
+
     fun example1() {
+        val subject = PublishSubject.create<Unit>()
+        val observable = subject.hide()
+
+        observable
+            .map {
+                if (firstExecution) {
+                    firstExecution = false
+                } else {
+                    throw Throwable("wtf")
+                }
+            }
+            .onErrorResumeNext {
+                println("onErrorResumeNext")
+                Observable.empty()
+            }
+            .subscribe {
+                println("subscribe")
+            }
+
+        subject.onNext(Unit) // prints "subscribe".
+        subject.onNext(Unit) // prints "onErrorResumeNext".
+        subject.onNext(Unit) // does nothing.
+    }
+
+    fun example2() {
+        val subject = PublishSubject.create<Unit>()
+        subject.subscribe {
+            println("subscribe")
+        }
+        subject.onNext(Unit) // prints "subscribe".
+        subject.onNext(Unit) // prints "subscribe".
+    }
+
+    fun example3() {
         val subject: PublishSubject<Unit> = PublishSubject.create()
 
         val observable1: Observable<Unit> = subject

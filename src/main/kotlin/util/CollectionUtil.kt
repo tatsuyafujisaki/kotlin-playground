f3429437e06da25099461e4f4b271e519f1e1146
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-16 00:06:27 +0900

    Add countOccurrences(...)

--- src/main/kotlin/util/CollectionUtil.kt
+++ src/main/kotlin/util/CollectionUtil.kt
@@ -4,4 +4,6 @@ object CollectionUtil {
     fun <T> addOrRemove(xs: Collection<T>?, x: T) = xs?.run {
         if (any { it == x }) minus(x) else plus(x)
     }
+
+    fun <T> countOccurrences(xs: Collection<T>) = xs.groupingBy { it }.eachCount()
 }

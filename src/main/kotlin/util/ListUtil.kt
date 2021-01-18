a8b89065fb4c126c211c3ff2b8ddbf7679c9e17e
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-18 22:41:37 +0900

    Add List<T>.rotateLeft(...)

--- src/main/kotlin/util/ListUtil.kt
+++ src/main/kotlin/util/ListUtil.kt
@@ -3,6 +3,8 @@ package util
 import java.util.Collections
 
 object ListUtil {
+    fun <T> List<T>.rotateLeft(distance: Int): List<T> = drop(distance) + take(distance)
+
     fun <T> List<T>.rotate(distance: Int): List<T> {
         with(toList()) {
             Collections.rotate(this, distance)

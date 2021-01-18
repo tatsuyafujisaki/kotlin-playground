62e6cb4fc40add237037e4b5cdabc2ca3b7d689d
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-19 00:29:24 +0900

    Update StringUtil.kt

--- src/main/kotlin/util/StringUtil.kt
+++ src/main/kotlin/util/StringUtil.kt
@@ -4,4 +4,13 @@ object StringUtil {
     fun String?.isNeitherNullNorEmpty(): Boolean = this != null && isNotEmpty()
     fun String?.isNeitherNullNorBlank(): Boolean = this != null && isNotBlank()
     fun String.equalsIgnoreCase(s2: String) = equals(s2, true)
+    fun String.alphabetized() = toCharArray().sorted().joinToString("")
+    
+    fun String.substrings() = sequence {
+        for (i in indices) {
+            for (j in i + 1..length) {
+                yield(substring(i, j))
+            }
+        }
+    }.toList()
 }

726a16b293224f35ede448aaf2cb29a8a03c72ab
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-21 00:34:17 +0900

    Update MapUtil.kt

--- src/main/kotlin/util/MapUtil.kt
+++ src/main/kotlin/util/MapUtil.kt
@@ -1,7 +1,8 @@
 package util
 
 object MapUtil {
-    fun <T> MutableMap<T, Int>.increment(key: T) = merge(key, 1, Int::plus)
+    fun <T> MutableMap<T, Int>.increment(key: T, value: Int = 1) = merge(key, value, Int::plus)
+    fun <T> MutableMap<T, Long>.increment(key: T, value: Long = 1) = merge(key, value, Long::plus)
     fun <T> Map<T, Int>.sortByValue() = toList().sortedBy { it.second }.toMap()
     fun <T> Map<T, Int>.sortByValueDescending() = toList().sortedByDescending { it.second }.toMap()
     fun <T> Map<T, Int>.getEntryOfMaxValue() = maxByOrNull { it.value }

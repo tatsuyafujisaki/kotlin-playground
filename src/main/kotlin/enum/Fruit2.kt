d43f5be5c4f85092d94e4e6db49728df6094be5a
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-21 21:25:52 +0900

    Update Fruit2.kt

--- src/main/kotlin/enum/Fruit2.kt
+++ src/main/kotlin/enum/Fruit2.kt
@@ -2,28 +2,29 @@ package enum
 
 enum class Fruit2 {
     APPLE {
-        override val color: String = "Red"
         override fun toString() = "🍎"
+        override val producer: String = "👨‍🌾"
         override fun printSimilarFruit() {
             println("🍏")
         }
     },
     ORANGE {
-        override val color: String = "Orange"
         override fun toString() = "🍊"
+        override val producer: String = "👩‍🌾"
         override fun printSimilarFruit() {
             println("🍋")
         }
     };
 
-    abstract val color: String
+    abstract val producer: String
     abstract fun printSimilarFruit()
 }
 
 object Fruit2Examples {
     fun example() {
         val apple: Fruit2 = Fruit2.APPLE
-        println(apple.color) // Red
+        println(apple) // 🍎
+        println(apple.producer) // 👨‍🌾
         apple.printSimilarFruit() // 🍏
     }
 }

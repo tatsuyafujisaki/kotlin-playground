d4000db6572a290ab1904719cd27077c3d684c40
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-22 20:42:13 +0900

    Update MapUtil.kt

--- src/main/kotlin/util/MapUtil.kt
+++ src/main/kotlin/util/MapUtil.kt
@@ -1,10 +1,29 @@
 package util
 
 object MapUtil {
-    fun <T> MutableMap<T, Int>.increment(key: T, value: Int = 1) = merge(key, value, Int::plus)
-    fun <T> MutableMap<T, Long>.increment(key: T, value: Long = 1) = merge(key, value, Long::plus)
-    fun <T> MutableMap<T, Int>.decrement(key: T, value: Int = 1) = merge(key, value, Int::minus)
-    fun <T> MutableMap<T, Long>.decrement(key: T, value: Long = 1) = merge(key, value, Long::minus)
+    fun <T> MutableMap<T, Int>.increment(key: T, value: Int = 1) {
+        merge(key, value, Int::plus)
+    }
+
+    fun <T> MutableMap<T, Long>.increment(key: T, value: Long = 1) {
+        merge(key, value, Long::plus)
+    }
+
+    fun <T> MutableMap<T, Int>.decrement(key: T, value: Int = 1) {
+        merge(key, value, Int::minus)
+    }
+
+    fun <T> MutableMap<T, Long>.decrement(key: T, value: Long = 1) {
+        merge(key, value, Long::minus)
+    }
+
+    fun <T> MutableMap<T, Int>.decrementIfPresent(key: T, value: Int = 1) {
+        merge(key, value, Int::minus)
+        if (this[key]!! < 0) {
+            remove(key)
+        }
+    }
+
     fun <T> Map<T, Int>.sortByValue() = toList().sortedBy { it.second }.toMap()
     fun <T> Map<T, Int>.sortByValueDescending() = toList().sortedByDescending { it.second }.toMap()
     fun <T> Map<T, Int>.getEntryOfMaxValue() = maxByOrNull { it.value }

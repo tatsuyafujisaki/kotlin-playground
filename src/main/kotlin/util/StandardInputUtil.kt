029311bfcf8bd9f50b80f46f0bcfc795cab4aad7
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-23 00:49:31 +0900

    Update StandardInputUtil.kt

--- src/main/kotlin/util/StandardInputUtil.kt
+++ src/main/kotlin/util/StandardInputUtil.kt
@@ -1,6 +1,6 @@
 package util
 
 object StandardInputUtil {
-    fun readIntegers() = readLine().orEmpty().split(' ').filterNot { it.isEmpty() }.map { it.toInt() }
-    fun readDoubles() = readLine().orEmpty().split(' ').filterNot { it.isEmpty() }.map { it.toDouble() }
+    fun readIntegers() = readLine().orEmpty().split(' ').filterNot(String::isEmpty).map(String::toInt)
+    fun readDoubles() = readLine().orEmpty().split(' ').filterNot(String::isEmpty).map(String::toDouble)
 }

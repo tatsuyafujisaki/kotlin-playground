28b89dde25c4664b40a03054b6184ae88894c04c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-22 23:56:48 +0900

    Update BubbleSort.kt

--- src/main/kotlin/sort/BubbleSort.kt
+++ src/main/kotlin/sort/BubbleSort.kt
@@ -5,8 +5,8 @@ object BubbleSort {
         var n = xs.size
         while (1 < n) {
             var lastSwappedIndex = 0
-            for (i in 0..(n - 2)) {
-                if (xs[i + 1] < xs[i]) {
+            for (i in 0 until n - 1) {
+                if (xs[i] > xs[i + 1]) {
                     xs.swap(i, i + 1)
                     lastSwappedIndex = i + 1
                 }

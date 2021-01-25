9ad7a641c8a70c7a558507b789d3f12fb0f5acf9
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-25 22:30:22 +0900

    Add readInt()

--- src/main/kotlin/util/CollectionUtil.kt
+++ src/main/kotlin/util/CollectionUtil.kt
@@ -1,10 +1,8 @@
 package util
 
 object CollectionUtil {
-    fun <T> addOrRemove(xs: Collection<T>?, x: T) = xs?.run {
-        if (any { it == x }) minus(x) else plus(x)
-    }
-
+    fun Collection<*>.separateLast() = with(chunked(size - 1)) { first() to last().last() }
+    fun <T> addOrRemove(xs: Collection<T>?, x: T) = xs?.run { if (any { it == x }) minus(x) else plus(x) }
     fun Collection<*>.countOccurrences() = groupingBy { it }.eachCount()
     fun Collection<*>.hasDuplicates() = this.groupingBy { it }.eachCount().any { it.value > 1 }
     fun Collection<*>.rotateLeft(distance: Int): List<*> = drop(distance) + take(distance)

3206b9be596513b569b80a428c48f0b6b60de15f
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-01-25 23:52:59 +0900

    Make print functions safer

--- src/main/kotlin/util/StandardOutputUtil.kt
+++ src/main/kotlin/util/StandardOutputUtil.kt
@@ -18,30 +18,38 @@ object StandardOutputUtil {
     }
 
     fun IntArray.printHorizontally() {
-        for (i in 0 until size - 1) {
-            print("${this[i]} ")
+        if (isNotEmpty()) {
+            for (i in 0 until size - 1) {
+                print("${this[i]} ")
+            }
+            println(this[size - 1])
         }
-        println(this[size - 1])
     }
 
     fun DoubleArray.printHorizontally() {
-        for (i in 0 until size - 1) {
-            print("${this[i]} ")
+        if (isNotEmpty()) {
+            for (i in 0 until size - 1) {
+                print("${this[i]} ")
+            }
+            println(this[size - 1])
         }
-        println(this[size - 1])
     }
 
     fun Array<*>.printHorizontally() {
-        for (i in 0 until size - 1) {
-            print("${this[i]} ")
+        if (isNotEmpty()) {
+            for (i in 0 until size - 1) {
+                print("${this[i]} ")
+            }
+            println(this[size - 1])
         }
-        println(this[size - 1])
     }
 
     fun List<*>.printHorizontally() {
-        for (i in 0 until size - 1) {
-            print("${this[i]} ")
+        if (isNotEmpty()) {
+            for (i in 0 until size - 1) {
+                print("${this[i]} ")
+            }
+            println(this[size - 1])
         }
-        println(this[size - 1])
     }
 }

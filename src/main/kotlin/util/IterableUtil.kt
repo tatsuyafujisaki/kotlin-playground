e83068e4f8ced0f187cf03f1996ee57e0ceeed77
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-03 21:53:56 +0900

    Add myScan() and myRunningReduce()

--- src/main/kotlin/util/IterableUtil.kt
+++ src/main/kotlin/util/IterableUtil.kt
@@ -6,4 +6,29 @@ object IterableUtil {
     fun Iterable<*>.hasDuplicates() = this.groupingBy { it }.eachCount().any { it.value > 1 }
     fun Iterable<*>.rotateLeft(distance: Int): List<*> = drop(distance) + take(distance)
     fun Iterable<Int>.prefixSum() = scan(0) { acc, x -> acc + x }
+
+    /** For a competitive programming platform that does not support scan() of Kotlin 1.4 */
+    fun <T, R> Iterable<T>.myScan(
+        initial: R,
+        operation: (acc: R, T) -> R
+    ): List<R> {
+        val result = mutableListOf(initial)
+        forEachIndexed { i, x ->
+            result.add(operation(result[i], x))
+        }
+        return result
+    }
+
+    /** For a competitive programming platform that does not support runningReduce() of Kotlin 1.4 */
+    fun <S, T : S> Iterable<T>.myRunningReduce(operation: (acc: S, T) -> S): List<S> {
+        val iterator = iterator()
+        if (!iterator.hasNext()) return emptyList()
+        var acc: S = iterator.next()
+        val result = mutableListOf(acc)
+        while (iterator.hasNext()) {
+            acc = operation(acc, iterator.next())
+            result.add(acc)
+        }
+        return result
+    }
 }

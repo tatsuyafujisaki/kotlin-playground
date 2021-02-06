e38834ffffaed4c2555a0386b14f44b800e4fb4e
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-07 00:35:02 +0900

    Update CharUtil.kt

--- src/main/kotlin/util/CharUtil.kt
+++ src/main/kotlin/util/CharUtil.kt
@@ -2,4 +2,10 @@ package util
 
 object CharUtil {
     val Char.alphabeticalIndex: Int get() = this - 'a'
+
+    /**
+     * '1'.toInt() -> 49
+     * '1'.toIntAsDigit() -> 1
+     */
+    fun Char.toIntAsDigit() = toString().toInt()
 }

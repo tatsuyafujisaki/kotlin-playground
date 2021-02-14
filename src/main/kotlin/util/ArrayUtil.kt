0a40522680ef786b9336ffd4adde562d8e5069c3
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-15 01:49:32 +0900

    Update ArrayUtil.kt

--- src/main/kotlin/util/ArrayUtil.kt
+++ src/main/kotlin/util/ArrayUtil.kt
@@ -1,7 +1,7 @@
 package util
 
 object ArrayUtil {
-    val pow2: IntArray = intArrayOf(
+    val pow2 = intArrayOf(
         1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
         1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288
     )

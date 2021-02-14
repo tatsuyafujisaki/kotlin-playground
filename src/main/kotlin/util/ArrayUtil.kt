66012ba9a188115f0e7bec5c6c1cad7776b971e7
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-15 01:21:00 +0900

    Add pow2

--- src/main/kotlin/util/ArrayUtil.kt
+++ src/main/kotlin/util/ArrayUtil.kt
@@ -1,6 +1,11 @@
 package util
 
 object ArrayUtil {
+    val pow2: IntArray = intArrayOf(
+        1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
+        1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288
+    )
+
     fun IntArray.swap(i: Int, j: Int) {
         val temp = this[i]
         this[i] = this[j]

4c052d16959c8fba3feed791f537bb41551f8022
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-15 21:03:12 +0900

    Update StackUtil.kt

--- src/main/kotlin/util/StackUtil.kt
+++ src/main/kotlin/util/StackUtil.kt
@@ -3,5 +3,5 @@ package util
 import java.util.Stack
 
 object StackUtil {
-    fun Stack<*>.safePop() = if (empty()) null else pop()
+    fun Stack<*>.safePop() = if (empty()) null else pop() // avoids an EmptyStackException.
 }

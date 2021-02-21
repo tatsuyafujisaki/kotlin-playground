776f952d7ba61d5aec8af7fc084a9b7a16e5d670
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-21 21:39:52 +0900

    Update SingleExamples.kt

--- src/main/kotlin/rx/SingleExamples.kt
+++ src/main/kotlin/rx/SingleExamples.kt
@@ -1,38 +1,16 @@
 package rx
 
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import rx.DisposeUtil.print
 
-/**
- * There is no such thing as Single#doOnNext(...) and Single#doOnComplete(...)
- * Moreover, Single#subscribe() does not take onNext(...) and onComplete(...).
- */
 object SingleExamples {
-    private val compositeDisposable = CompositeDisposable()
     private var observerCount = 0
 
-    fun example(item: String = "apple") {
-        val single = Single
-            .just(item)
-            // .map { throw Throwable("WTF") }
-            .doOnSuccess {
-                println("doOnSuccess: $it")
-            }
-            .doOnError {
-                println("doOnError: ${it.message}")
-                it.printStackTrace()
-            }
-            .onErrorResumeNext {
-                println("onErrorResumeNext: ${it.message}")
-                it.printStackTrace()
-                Single.never()
-            }
-        with(single) {
-            compositeDisposable.add(mySubscribe())
-            compositeDisposable.add(mySubscribe())
-        }
-        compositeDisposable.clear()
+    fun example() {
+        val single = Single.just("a")
+        val disposable = single.mySubscribe()
+        disposable.print() // true
     }
 
     fun Single<*>.mySubscribe(): Disposable {

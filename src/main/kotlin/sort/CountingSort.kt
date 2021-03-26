711238b8d27472657684aa762e7d48fc68da3953
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-27 00:46:24 +0900

    Update CountingSort.kt

--- src/main/kotlin/sort/CountingSort.kt
+++ src/main/kotlin/sort/CountingSort.kt
@@ -3,9 +3,7 @@ package sort
 object CountingSort {
     fun sort(xs: IntArray, max: Int = xs.maxOrNull()!!) {
         val counts = IntArray(max + 1)
-        for (x in xs) {
-            counts[x]++
-        }
+        for (x in xs) counts[x]++
         var i = 0
         counts.forEachIndexed { j, x ->
             repeat(x) {

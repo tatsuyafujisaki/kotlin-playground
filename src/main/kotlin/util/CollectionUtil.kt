3688865234261cd3be834b73d0d1085b4a23104d
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-29 18:19:39 +0900

    Update CollectionUtil.kt

--- src/main/kotlin/util/CollectionUtil.kt
+++ src/main/kotlin/util/CollectionUtil.kt
@@ -1,7 +1,10 @@
 package util
 
 object CollectionUtil {
-    fun <T> Collection<T>.permute(result: List<T> = emptyList()): List<List<T>> =
-        if (isEmpty()) listOf(result) else flatMap { (this - it).permute(result + it) }
+    fun <T> Collection<T>.permute(result: List<T> = emptyList()): List<List<T>> {
+        fun <T> Collection<T>.permute(result: List<T> = emptyList()): List<List<T>> =
+            if (isEmpty()) listOf(result) else flatMap { (this - it).permute(result + it) }
+        return permute()
+    }
     fun <T> Collection<T>.permuteWithoutDuplicates() = permute().distinct()
 }

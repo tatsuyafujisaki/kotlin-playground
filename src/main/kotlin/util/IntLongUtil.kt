2b5b2c3c56c8d0fcd909f2450cdb06a2598b1dee
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-29 09:44:31 +0900

    Update IntLongUtil.kt

--- src/main/kotlin/util/IntLongUtil.kt
+++ src/main/kotlin/util/IntLongUtil.kt
@@ -7,21 +7,49 @@ import kotlin.math.sqrt
 import util.ListUtil.permute
 
 object IntLongUtil {
-    fun Int.between(fromInclusive: Int, toExclusive: Int) = this in fromInclusive until toExclusive
+    val Int.isEven get() = this % 2 == 0
+    val Int.isSquareNumber get() = sqrt(toDouble()) % 1.0 == 0.0
 
-    /** If overflowed, returns Int.MAX_VALUE. */
-    fun Int.pow(n: Int) = toDouble().pow(n).toInt()
+    /** O(sqrt(n)) */
+    val Int.isPrime: Boolean
+        get() {
+            if (this == 2) return true
+            if (this < 2 || this % 2 == 0) return false
+            for (i in 3..sqrt(toDouble()).toInt() step 2) {
+                if (this % i == 0) return false
+            }
+            return true
+        }
+
+    val Int.largestDivisor: Int
+        get() {
+            for (i in 2..this) {
+                if (this % i == 0) return this / i
+            }
+            return 1
+        }
+
+    val Int.nth: String
+        get() {
+            val lastDigit = this % 10
+            return toString() + when {
+                this in 11..13 || lastDigit !in 1..3 -> "th"
+                lastDigit == 1 -> "st"
+                lastDigit == 2 -> "nd"
+                lastDigit == 3 -> "rd"
+                else -> error("Impossible state")
+            }
+        }
+
+    fun Int.sqrtInt() = sqrt(toDouble()).toInt()
+    fun Int.permute() = (1..this).toList().permute()
+    fun Int.between(fromInclusive: Int, toExclusive: Int) = this in fromInclusive until toExclusive
+    fun Int.pow(n: Int) = toDouble().pow(n).toInt() // returns Int.MAX_VALUE when it overflowed.
     fun Int.pow(n: Long) = toDouble().pow(n.toInt()).toLong()
     fun Long.pow(n: Int) = toDouble().pow(n).toLong()
     fun Long.pow(n: Long) = toDouble().pow(n.toInt()).toLong()
-
     fun Long.divideAndCeil(divisor: Long) = ceil(toDouble() / divisor).toLong()
     fun Long.divideAndFloor(divisor: Long) = floor(toDouble() / divisor).toLong()
-
-    fun Int.isEven() = this % 2 == 0
-    fun Int.isSquareNumber() = sqrt(toDouble()) % 1.0 == 0.0
-    fun Int.sqrtInt() = sqrt(toDouble()).toInt()
-    fun Int.permute() = (1..this).toList().permute()
     fun divMod(a: Int, b: Int) = a / b to a % b
 
     /**
@@ -38,34 +66,6 @@ object IntLongUtil {
 
     fun nPr(n: Int, r: Int) = n.factorial().toDouble() / (n - r).factorial()
     fun nCr(n: Int, r: Int) = nPr(n, r) / r.factorial()
-
-    // O(sqrt(n))
-    fun Int.isPrime(): Boolean {
-        if (this == 2) return true
-        if (this < 2 || this % 2 == 0) return false
-        for (i in 3..sqrt(toDouble()).toInt() step 2) {
-            if (this % i == 0) return false
-        }
-        return true
-    }
-
-    fun Int.largestDivisor(): Int {
-        for (i in 2..this) {
-            if (this % i == 0) return this / i
-        }
-        return 1
-    }
-
-    fun Int.nth(): String {
-        val lastDigit = this % 10
-        return toString() + when {
-            this in 11..13 || lastDigit !in 1..3 -> "th"
-            lastDigit == 1 -> "st"
-            lastDigit == 2 -> "nd"
-            lastDigit == 3 -> "rd"
-            else -> error("Impossible state")
-        }
-    }
 
     /** @return 1, 2, ..., or 6, if sides is 6 */
     fun rollDice(sides: Int = 6) = (1..sides).random()

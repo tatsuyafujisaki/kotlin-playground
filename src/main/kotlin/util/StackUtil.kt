27d1fc1ed4c205cdf450c628ad47c8b1ba3b8801
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-31 17:35:29 +0900

    Update StackUtil.kt

--- src/main/kotlin/util/StackUtil.kt
+++ src/main/kotlin/util/StackUtil.kt
@@ -4,4 +4,9 @@ import java.util.Stack
 
 object StackUtil {
     fun Stack<*>.popOrNull() = if (empty()) null else pop() // avoids an EmptyStackException.
+
+    /** Redundant wrapper */
+    fun Stack<*>.peekOrNull() = lastOrNull()
+
+    fun <T> Stack<T>.peekOrElse(item: T) = lastOrNull() ?: item
 }

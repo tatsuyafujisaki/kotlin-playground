33922eb8a165298e35e84ef03625168a710f8c39
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-01 23:35:16 +0900

    Update IterableUtil.kt

--- src/main/kotlin/util/IterableUtil.kt
+++ src/main/kotlin/util/IterableUtil.kt
@@ -1,9 +1,10 @@
 package util
 
 object IterableUtil {
-    fun <T> addOrRemove(xs: Iterable<T>?, x: T) = xs?.run { if (any { it == x }) minus(x) else plus(x) }
-    fun Iterable<*>.counts() = groupingBy { it }.eachCount()
-    fun Iterable<*>.hasDuplicates() = this.groupingBy { it }.eachCount().any { it.value > 1 }
+    val Iterable<*>.head get() = first()
+    val Iterable<*>.tail get() = drop(1)
+    fun <T> Iterable<T>.addOrRemove(x: T) = run { if (any { it == x }) minus(x) else plus(x) }
+    fun Iterable<*>.hasDuplicates() = groupingBy { it }.eachCount().any { it.value > 1 }
     fun Iterable<*>.rotateLeft(distance: Int): List<*> = drop(distance) + take(distance)
     fun Iterable<Int>.prefixSum() = scan(0) { acc, x -> acc + x }
 

dd0ecc7158467ca94c43ab2a9f664fc459f667f0
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 16:36:54 +0900

    Update TreeSample.kt

--- src/main/kotlin/algorithm/TreeSample.kt
+++ src/main/kotlin/algorithm/TreeSample.kt
@@ -1,23 +1,26 @@
 package algorithm
 
 object TreeSample {
-    class Vertex(val parent: Vertex?, val id: Int, val data: Int, var children: Set<Vertex>)
+    class Vertex(val id: Int, val data: Int, var children: Set<Vertex>)
 
-    fun convertUndirectedGraphToTree(undirectedGraph: List<Set<Int>>, data: List<Int>, root: Int): Vertex {
-        val visited = BooleanArray(undirectedGraph.size)
+    fun convertUndirectedGraphToTree(
+        undirectedGraph: List<Set<Int>>,
+        data: List<Int>,
+        root: Int
+    ): Pair<IntArray, Vertex> {
+        val parents = IntArray(undirectedGraph.size) { Int.MIN_VALUE }
 
-        fun createVertex(parent: Vertex?, id: Int): Vertex {
-            visited[id] = true
-            return Vertex(parent, id, data[id], emptySet()).apply {
-                children = undirectedGraph[id]
-                    .filterNot {
-                        visited[it]
-                    }.map {
-                        createVertex(this, it)
-                    }.toSet()
-            }
+        fun createVertex(parent: Int, id: Int): Vertex {
+            parents[id] = parent
+            return Vertex(id, data[id], undirectedGraph[id]
+                .filter {
+                    parents[it] == Int.MIN_VALUE
+                }.map {
+                    createVertex(id, it)
+                }.toSet()
+            )
         }
 
-        return createVertex(null, root)
+        return parents to createVertex(-1, root)
     }
 }

4a5848bcf181dd004ecdb8523ffc992b2b0ddb36
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-18 08:21:29 +0900

    Update SimulatedStandardInput.kt

--- src/main/kotlin/util/SimulatedStandardInput.kt
+++ src/main/kotlin/util/SimulatedStandardInput.kt
@@ -3,6 +3,10 @@ package util
 import java.io.File
 
 /**
+ * Usage:
+ * val stdin = SimulatedStandardInput("inpout.txt")
+ * fun readLine() = stdin.readLine()
+ *
  * @param pathname is expected to exist as the src folder's sibling.
  */
 class SimulatedStandardInput(pathname: String) {

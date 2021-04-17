22c3283801829f4676fd9febcd216c91f3dd43d0
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 14:14:06 +0900

    Create Tree.kt

---
+++ src/main/kotlin/algorithm/Tree.kt
@@ -0,0 +1,21 @@
+package algorithm
+
+class Vertex(val parent: Vertex?, val value: Int, var children: Set<Vertex>)
+
+fun convertUndirectedGraphToTree(undirectedGraph: List<Set<Int>>, root: Int): Vertex {
+    val visited = BooleanArray(undirectedGraph.size)
+
+    fun createVertex(parent: Vertex?, value: Int): Vertex {
+        visited[value] = true
+        return Vertex(parent, value, emptySet()).apply {
+            children = undirectedGraph[value]
+                .filterNot {
+                    visited[it]
+                }.map {
+                    createVertex(this, it)
+                }.toSet()
+        }
+    }
+
+    return createVertex(null, root)
+}

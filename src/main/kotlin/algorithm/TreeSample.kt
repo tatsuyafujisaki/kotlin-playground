98ef29278c052671ffe1f432d6664828238d85d2
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-17 23:06:49 +0900

    Delete TreeSample2 and TreeSample3

--- src/main/kotlin/algorithm/TreeSample.kt
+++ src/main/kotlin/algorithm/TreeSample.kt
@@ -1,26 +1,45 @@
 package algorithm
 
+data class Tree(val ancestors: List<List<Int>>, val children: List<Set<Int>>, val subtreeData: List<Long>)
+
 object TreeSample {
-    class Vertex(val id: Int, val data: Int, var children: Set<Vertex>)
-
-    fun convertUndirectedGraphToTree(
+    fun convertUndirectedAcyclicGraphToTree(
         undirectedGraph: List<Set<Int>>,
-        data: List<Int>,
+        data: IntArray,
         root: Int
-    ): Pair<IntArray, Vertex> {
-        val parents = IntArray(undirectedGraph.size) { Int.MIN_VALUE }
+    ): Tree {
+        val n = undirectedGraph.size
+        val ancestors = Array<List<Int>>(n) { emptyList() }
+        val children = Array<Set<Int>>(n) { emptySet() }
+        val subtreeData = LongArray(n)
 
-        fun createVertex(parent: Int, id: Int): Vertex {
-            parents[id] = parent
-            return Vertex(id, data[id], undirectedGraph[id]
-                .filter {
-                    parents[it] == Int.MIN_VALUE
-                }.map {
-                    createVertex(id, it)
-                }.toSet()
-            )
+        fun visitVertex(parent: Int, id: Int) {
+            if (parent != -1) ancestors[id] = listOf(parent) + ancestors[parent]
+            children[id] = undirectedGraph[id].filter { it != root && ancestors[it].isEmpty() }.toSet()
+            for (child in children[id]) visitVertex(id, child)
+            subtreeData[id] = data[id] + children[id].map { subtreeData[it] }.sum()
         }
 
-        return parents to createVertex(-1, root)
+        visitVertex(-1, root)
+
+        return Tree(ancestors.toList(), children.toList(), subtreeData.toList())
+    }
+
+    /** finds the lowest common ancestor (LCA). */
+    fun findLCA(ancestors: Array<List<Int>>, vertex1: Int, vertex2: Int): Int {
+        var v1 = vertex1
+        var v2 = vertex2
+        while (ancestors[v1].size != ancestors[v2].size) {
+            if (ancestors[v1].size < ancestors[v2].size) {
+                v2 = ancestors[v2].first()
+            } else {
+                v1 = ancestors[v1].first()
+            }
+        }
+        while (v1 != v2) {
+            v1 = ancestors[v1].first()
+            v2 = ancestors[v2].first()
+        }
+        return v1
     }
 }

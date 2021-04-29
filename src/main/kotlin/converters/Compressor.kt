090e270e5a827ad260109e609ae9c09e68a3dff2
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-29 22:17:58 +0900

    Update Compressor.kt

--- src/main/kotlin/converters/Compressor.kt
+++ src/main/kotlin/converters/Compressor.kt
@@ -4,8 +4,8 @@ import java.util.SortedSet
 
 class Compressor<T : Comparable<T>>(originals: SortedSet<T>) {
     val compressed = originals.indices
-    private val compressMap = originals.withIndex().associate { it.value to it.index }.toMap()
-    private val decompressMap = originals.withIndex().associate { it.index to it.value }.toMap()
+    private val compressMap = originals.mapIndexed { i, x -> x to i }.toMap()
+    private val decompressMap = originals.mapIndexed { i, x -> i to x }.toMap()
     fun compress(original: T) = compressMap[original]!!
     fun decompress(compressed: Int) = decompressMap[compressed]!!
 }

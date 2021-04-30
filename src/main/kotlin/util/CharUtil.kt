11130c8350527676d1a8f04e18d4d154728aa164
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-05-01 01:12:56 +0900

    Update CharUtil.kt

--- src/main/kotlin/util/CharUtil.kt
+++ src/main/kotlin/util/CharUtil.kt
@@ -2,14 +2,5 @@ package util
 
 object CharUtil {
     val Char.alphabeticalIndex get() = this - 'a'
-
-    /**
-     * '1'.toInt() -> 49
-     * '1'.digitToInt() -> 1
-     */
-    @Deprecated(
-        message = "Use Char.digitToInt() in Kotlin 1.4.30+. A Java alternative is Character.getNumericValue().",
-        replaceWith = ReplaceWith("Char.digitToInt()")
-    )
-    fun Char.myDigitToInt() = toString().toInt()
+    val Char.digit get() = this - '0' // Use Char.digitToInt() in Kotlin 1.4.30+.
 }

1edb2e96f06539e3a2a7030747fc9c5e77f2ad1e
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-11 00:54:05 +0900

    Update enums

--- src/main/kotlin/enum/Fruit.kt
+++ src/main/kotlin/enum/Fruit.kt
@@ -1,12 +1,15 @@
 package enum
 
 enum class Fruit {
-    APPLE,
-    ORANGE;
+    APPLE, ORANGE, UNKNOWN;
 
     companion object {
-        fun fromName(name: String): Fruit? = values().find { it.name == name }
         fun fromOrdinal(ordinal: Int): Fruit = values()[ordinal]
         fun fromOrdinalOrNull(ordinal: Int): Fruit? = values().getOrNull(ordinal)
+        fun fromOrdinalWithDefault(ordinal: Int): Fruit = values().getOrElse(ordinal) { UNKNOWN }
+        fun fromName(name: String): Fruit = valueOf(name)
+        fun fromNameIgnoreCase(name: String): Fruit = valueOf(name.uppercase())
+        fun fromNameOrNull(name: String): Fruit? = values().find { it.name == name }
+        fun fromNameOrNullIgnoreCase(name: String): Fruit? = values().find { it.name == name.uppercase() }
     }
 }

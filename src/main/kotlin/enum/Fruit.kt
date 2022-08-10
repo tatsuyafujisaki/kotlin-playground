c4006657471e5e11c5f01de5156e94beccb76e8b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-08-11 01:07:54 +0900

    Update Fruit

--- src/main/kotlin/enum/Fruit.kt
+++ src/main/kotlin/enum/Fruit.kt
@@ -4,11 +4,11 @@ enum class Fruit {
     APPLE, ORANGE, UNKNOWN;
 
     companion object {
-        fun fromOrdinal(ordinal: Int): Fruit = values()[ordinal]
+        fun fromOrdinalOrThrow(ordinal: Int): Fruit = values()[ordinal]
         fun fromOrdinalOrNull(ordinal: Int): Fruit? = values().getOrNull(ordinal)
-        fun fromOrdinalWithDefault(ordinal: Int): Fruit = values().getOrElse(ordinal) { UNKNOWN }
-        fun fromName(name: String): Fruit = valueOf(name)
-        fun fromNameIgnoreCase(name: String): Fruit = valueOf(name.uppercase())
+        fun fromOrdinalOrDefault(ordinal: Int): Fruit = values().getOrElse(ordinal) { UNKNOWN }
+        fun fromNameOrThrow(name: String): Fruit = valueOf(name)
+        fun fromNameIgnoreCaseOrThrow(name: String): Fruit = valueOf(name.uppercase())
         fun fromNameOrNull(name: String): Fruit? = values().find { it.name == name }
         fun fromNameIgnoreCaseOrNull(name: String): Fruit? = values().find { it.name == name.uppercase() }
     }
--- src/main/kotlin/enum/MyColor.kt
+++ src/main/kotlin/enum/MyColor.kt
@@ -4,14 +4,14 @@ enum class MyColor(val value: Int) {
     BLACK(0x000000), WHITE(0xffffff), UNKNOWN(Int.MIN_VALUE);
 
     companion object {
-        fun fromOrdinal(ordinal: Int): MyColor = values()[ordinal]
+        fun fromOrdinalOrThrow(ordinal: Int): MyColor = values()[ordinal]
         fun fromOrdinalOrNull(ordinal: Int): MyColor? = values().getOrNull(ordinal)
-        fun fromOrdinalWithDefault(ordinal: Int): MyColor = values().getOrElse(ordinal) { UNKNOWN }
-        fun fromName(name: String): MyColor = valueOf(name)
-        fun fromNameIgnoreCase(name: String): MyColor = valueOf(name.uppercase())
+        fun fromOrdinalOrDefault(ordinal: Int): MyColor = values().getOrElse(ordinal) { UNKNOWN }
+        fun fromNameOrThrow(name: String): MyColor = valueOf(name)
+        fun fromNameIgnoreCaseOrThrow(name: String): MyColor = valueOf(name.uppercase())
         fun fromNameOrNull(name: String): MyColor? = values().find { it.name == name }
         fun fromNameIgnoreCaseOrNull(name: String): MyColor? = values().find { it.name == name.uppercase() }
-        fun fromValue(value: Int): MyColor = values().first { it.value == value }
+        fun fromValueOrThrow(value: Int): MyColor = values().first { it.value == value }
         fun fromValueOrNull(value: Int): MyColor? = values().find { it.value == value }
     }
 }

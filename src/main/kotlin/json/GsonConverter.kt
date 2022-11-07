b16304ad34502dd139ab4cea2eb904f916629cb9
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-11-07 19:51:02 +0900

    Integrate GsonConverterSamples into GsonConverter

--- src/main/kotlin/json/GsonConverter.kt
+++ src/main/kotlin/json/GsonConverter.kt
@@ -2,9 +2,20 @@ package json
 
 import com.google.gson.Gson
 
-/** @see GsonConverterSamples */
 object GsonConverter {
     val gson = Gson()
-    fun <T> encodeToString(src: T): String = gson.toJson(src)
-    inline fun <reified T> decodeFromString(json: String): T = gson.fromJson(json, T::class.java)
+    fun <T> toString(src: T): String = gson.toJson(src)
+    inline fun <reified T> fromJson(json: String): T = gson.fromJson(json, T::class.java)
+}
+
+fun main() {
+    val person = Person("Jane", 18)
+    println(person)
+
+    val json = GsonConverter.toString(person)
+    println(json)
+
+    // Note that the returned value is not nullable unlike Moshi.
+    val person2: Person = GsonConverter.fromJson(json)
+    println(person2)
 }

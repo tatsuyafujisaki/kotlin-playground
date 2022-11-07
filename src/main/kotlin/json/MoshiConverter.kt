668ceb6ca4a55213acfc8f79a9b10a5442c0500b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-11-07 20:57:08 +0900

    Modify MoshiConverter

--- src/main/kotlin/json/MoshiConverter.kt
+++ src/main/kotlin/json/MoshiConverter.kt
@@ -1,22 +1,49 @@
 package json
 
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.io.File
 
 object MoshiConverter {
     val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     inline fun <reified T> toJson(value: T): String = moshi.adapter(T::class.java).toJson(value)
     inline fun <reified T> fromJson(string: String) = moshi.adapter(T::class.java).fromJson(string)
+
+    inline fun <reified T> getListAdapter(): JsonAdapter<List<T>> = moshi.adapter(
+        Types.newParameterizedType(
+            List::class.java, T::class.java
+        )
+    )
+
+    inline fun <reified T> getMapAdapter(): JsonAdapter<Map<String, T>> = moshi.adapter(
+        Types.newParameterizedType(
+            Map::class.java, String::class.java, T::class.java
+        )
+    )
+}
+
+fun sample1() {
+    val json = File("data/object-of-numbers.json").readText()
+    val map = MoshiConverter.getMapAdapter<Int>().fromJson(json)
+    println(map)
+}
+
+fun sample2() {
+    val json = File("data/object.json").readText()
+    val map = MoshiConverter.getMapAdapter<String>().fromJson(json)
+    println(map)
+}
+
+fun sample3() {
+    val json = File("data/array.json").readText()
+    val xs = MoshiConverter.getListAdapter<Int>().fromJson(json)
+    println(xs)
 }
 
 fun main() {
-    val person = Person("Jane", 18)
-    println(person)
-
-    val json = MoshiConverter.toJson(person)
-    println(json)
-
-    // Note that the returned value is nullable unlike Kotlin Serialization and Gson.
-    val person2: Person? = MoshiConverter.fromJson(json)
-    println(person2)
+    sample1()
+    sample2()
+    sample3()
 }

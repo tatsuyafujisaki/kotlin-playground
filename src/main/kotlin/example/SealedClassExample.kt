ad54c9df9c0518c40403cda041e627625780db8b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-11-30 19:10:35 +0900

    Update SealedClassExample

--- src/main/kotlin/example/SealedClassExample.kt
+++ src/main/kotlin/example/SealedClassExample.kt
@@ -1,17 +1,19 @@
 package example
 
 sealed class Shape(val description: String) {
-    val common: String = "shape's common description"
+    val common: String = "common!"
 }
 
-data class Circle(val radius: Int) : Shape("circle's description")
-data class Square(val perimeter: Int) : Shape("square's description")
+data class Circle(val radius: Int) : Shape("circle!")
+data class Square(val perimeter: Int) : Shape("square!")
 
 fun main() {
     val circle = Circle(1)
     val square = Square(1)
-    println(circle)
-    println(circle.common)
-    println(square)
-    println(square.common)
+    println(circle) // Circle(radius=1)
+    println(circle.common) // shape!
+    println(circle.description) // common!
+    println(square) // Square(perimeter=1)
+    println(square.common) // common!
+    println(square.description) // square!
 }
--- src/main/kotlin/example/SealedInterfaceExample.kt
+++ src/main/kotlin/example/SealedInterfaceExample.kt
@@ -5,18 +5,18 @@ sealed interface Shape2 {
 }
 
 data class Circle2(val radius: Int) : Shape2 {
-    override val description = "circle's description"
+    override val description = "circle!"
 }
 
 data class Square2(val perimeter: Int) : Shape2 {
-    override val description = "square's description"
+    override val description = "square!"
 }
 
 fun main() {
     val circle = Circle2(1)
     val square = Square2(1)
-    println(circle)
-    println(circle.description)
-    println(square)
-    println(square.description)
+    println(circle) // Circle2(radius=1)
+    println(circle.description) // circle!
+    println(square) // Square2(perimeter=1)
+    println(square.description) // square!
 }

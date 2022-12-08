35f10bf36ee9c7fdec064ad6e81fd1e824e51633
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-08 21:16:44 +0900

    Update SealedInterfaceExample.kt

--- src/main/kotlin/example/SealedInterfaceExample.kt
+++ src/main/kotlin/example/SealedInterfaceExample.kt
@@ -2,26 +2,26 @@ package example
 
 sealed interface Shape {
     val description: String
-    val onClick: () -> Unit
+    val print: () -> Unit
 }
 
 data class Circle(
     val radius: Int,
     override val description: String,
-    override val onClick: () -> Unit
+    override val print: () -> Unit
 ) : Shape
 
 data class Square(
     val perimeter: Int,
     override val description: String,
-    override val onClick: () -> Unit
+    override val print: () -> Unit
 ) : Shape
 
 fun main() {
-    val circle = Circle(1, "circle!") { println("Circle is clicked.") }
-    val square = Square(1, "square!") { println("Square is clicked.") }
-    println(circle) // Circle(radius=1, description=circle!, onClick=() -> kotlin.Unit)
-    circle.onClick() // Circle is clicked.
-    println(square) // Square(perimeter=1, description=square!, onClick=() -> kotlin.Unit)
-    square.onClick() // Square is clicked.
+    val circle = Circle(1, "circle!") { println("circle!") }
+    val square = Square(1, "square!") { println("square!") }
+    println(circle)
+    circle.print()
+    println(square)
+    square.print()
 }

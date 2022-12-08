c3762ea76e15b32790a9ba20fc3260392831f757
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-08 21:12:25 +0900

    Update SealedClassExample.kt

--- src/main/kotlin/example/SealedClassExample.kt
+++ src/main/kotlin/example/SealedClassExample.kt
@@ -1,31 +1,31 @@
 package example
 
-sealed class Shape2(open val description: String, open val onClick: () -> Unit) {
+sealed class Shape2(open val description: String, open val print: () -> Unit) {
     val commonDescription: String = "common!"
-    val onCommonClick: () -> Unit = { println("Clicked.") }
+    val printCommon: () -> Unit = { println("common!") }
 }
 
 data class Circle2(
     val radius: Int,
     override val description: String,
-    override val onClick: () -> Unit
-) : Shape2("circle!", onClick)
+    override val print: () -> Unit
+) : Shape2("circle!", print)
 
 data class Square2(
     val perimeter: Int,
     override val description: String,
-    override val onClick: () -> Unit
-) : Shape2("square!", onClick)
+    override val print: () -> Unit
+) : Shape2("square!", print)
 
 fun main() {
-    val circle = Circle2(1, "circle!") { println("Circle is clicked.") }
-    val square = Square2(1, "square!") { println("Square is clicked.") }
-    println(circle) // Circle2(radius=1, description=circle!, onClick=() -> kotlin.Unit)
-    println(circle.commonDescription) // Clicked.
-    circle.onCommonClick() // Circle is clicked.
-    circle.onClick() // Circle is clicked.
-    println(square) // Square2(perimeter=1, description=square!, onClick=() -> kotlin.Unit)
-    println(square.commonDescription) // common!
-    square.onCommonClick() // Clicked.
-    square.onClick() // Square is clicked.
+    val circle = Circle2(1, "circle!") { println("circle!") }
+    val square = Square2(1, "square!") { println("square!") }
+    println(circle)
+    println(circle.commonDescription)
+    circle.printCommon()
+    circle.print()
+    println(square)
+    println(square.commonDescription)
+    square.printCommon()
+    square.print()
 }

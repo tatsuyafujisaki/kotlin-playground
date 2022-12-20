1bde5055b64e7ab29c44a8e6430e7d75f829b21f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-20 16:45:59 +0900

    Create FlowCatchExample.kt

--- src/main/kotlin/flow/FlowExample.kt
+++ src/main/kotlin/flow/FlowExample.kt
@@ -1,17 +1,26 @@
 package flow
 
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 private suspend fun main(): Unit = coroutineScope {
-    flow {
-        emit("a")
-        emit("b")
-        emit("c")
-    }.catch {
-        println("catch: $it")
-    }.collect {
-        println(it)
+    val mutableFlow = MutableSharedFlow<String>()
+    val flow: Flow<String> = mutableFlow
+
+    launch {
+        flow.catch {
+            println("Catch: $it")
+        }.collect {
+            println("Collect: $it")
+        }
     }
+
+    delay(1000)
+    mutableFlow.emit("a")
+    mutableFlow.emit("b")
+    mutableFlow.emit("c")
 }

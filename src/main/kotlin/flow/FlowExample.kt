9e73ba347b2ceadc0ee5a2dd06f42ad51cb51671
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-20 16:51:26 +0900

    Update FlowExample.kt

--- src/main/kotlin/flow/FlowExample.kt
+++ src/main/kotlin/flow/FlowExample.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 private suspend fun main(): Unit = coroutineScope {
@@ -12,15 +14,21 @@ private suspend fun main(): Unit = coroutineScope {
     val flow: Flow<String> = mutableFlow
 
     launch {
-        flow.catch {
-            println("Catch: $it")
-        }.collect {
-            println("Collect: $it")
-        }
+        flow
+            .map {
+                if (it == "b") throw Throwable() else it
+            }.catch {
+                println("Catch: $it")
+                // Prevent the flow from completing.
+                emitAll(flow)
+            }.collect {
+                println("Collect: $it")
+            }
     }
 
     delay(1000)
     mutableFlow.emit("a")
     mutableFlow.emit("b")
+    delay(1000)
     mutableFlow.emit("c")
 }

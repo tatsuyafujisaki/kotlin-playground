05e99bb91152fb306c15e6d097423d16d275f264
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-20 17:02:04 +0900

    Update flow

--- src/main/kotlin/flow/MutableSharedFlowExample.kt
+++ src/main/kotlin/flow/MutableSharedFlowExample.kt
@@ -6,23 +6,21 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 
-private suspend fun main() {
-    coroutineScope {
-        val mutableFlow = MutableSharedFlow<String>()
-        val flow: Flow<String> = mutableFlow
+private suspend fun main() = coroutineScope {
+    val mutableFlow = MutableSharedFlow<String>()
+    val flow: Flow<String> = mutableFlow
 
-        launch {
-            flow.collect {
-                println("Collect: $it")
-            }
+    launch {
+        flow.collect {
+            println("Collect: $it")
         }
-
-        // Purposefully get delayed to ensure that a collector starts
-        // because the collector cannot collect a value emitted before the collector starts.
-        delay(1000)
-        mutableFlow.emit("a")
-        println("a is emitted.")
-        mutableFlow.emit("a")
-        println("a is emitted. MutableSharedFlow can collect the same value.")
     }
+
+    // Purposefully get delayed to ensure that a collector starts
+    // because the collector cannot collect a value emitted before the collector starts.
+    delay(1000)
+    mutableFlow.emit("a")
+    println("a is emitted.")
+    mutableFlow.emit("a")
+    println("a is emitted. MutableSharedFlow can collect the same value.")
 }
--- src/main/kotlin/flow/MutableStateFlowExample.kt
+++ src/main/kotlin/flow/MutableStateFlowExample.kt
@@ -5,22 +5,20 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-private suspend fun main() {
-    coroutineScope {
-        val mutableFlow = MutableStateFlow("a")
-        val flow: Flow<String> = mutableFlow
+private suspend fun main() = coroutineScope {
+    val mutableFlow = MutableStateFlow("a")
+    val flow: Flow<String> = mutableFlow
 
-        launch {
-            flow.collect {
-                println("Collect: $it")
-            }
+    launch {
+        flow.collect {
+            println("Collect: $it")
         }
-
-        // No need to get delayed to ensure that a collector starts
-        // because the collector can collect a value emitted before the collector starts.
-        mutableFlow.emit("a")
-        println("a is emitted.")
-        mutableFlow.emit("a")
-        println("a is emitted, but unlike MutableSharedFlow, MutableStateFlow CANNOT collect the same value.")
     }
+
+    // No need to get delayed to ensure that a collector starts
+    // because the collector can collect a value emitted before the collector starts.
+    mutableFlow.emit("a")
+    println("a is emitted.")
+    mutableFlow.emit("a")
+    println("a is emitted, but unlike MutableSharedFlow, MutableStateFlow CANNOT collect the same value.")
 }

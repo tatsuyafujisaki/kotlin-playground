37e82207130ba018e97dc007202549cce4b4b4dd
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-21 10:14:27 +0900

    Add onMisc to FlowUtil

--- src/main/kotlin/util/FlowUtil.kt
+++ src/main/kotlin/util/FlowUtil.kt
@@ -1,19 +1,28 @@
 package util
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.onStart
 
 object FlowUtil {
+    fun <T : Any> Flow<T>.onMisc() = onStart {
+        println("doOnSubscribe")
+    }.onEach {
+        println("onEach: $it")
+    }.onEmpty {
+        println("onEmpty") // completes without emitting any elements.
+    }.onCompletion {
+        println("onCompletion" + if (it == null) "without a throwable" else "with throwable: $it")
+    }
+
     fun <T> buildFlow(interval: Long = 1000, emitter: () -> T) = flow {
         while (true) {
             emit(emitter())
             delay(interval)
         }
-    }
-
-    fun timerFlow(interval: Long = 1000) = generateSequence(0L, Long::inc).asFlow().onEach {
-        delay(interval)
     }
 }

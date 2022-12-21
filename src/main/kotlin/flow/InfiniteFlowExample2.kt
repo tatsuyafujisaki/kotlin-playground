34cc830da7998b2dbe4dbe3817858d6c9b3b7531
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-12-21 10:09:39 +0900

    Add InfiniteFlowExample2

--- src/main/kotlin/flow/InfiniteFlowExample2.kt
+++ src/main/kotlin/flow/InfiniteFlowExample2.kt
@@ -2,15 +2,12 @@ package flow
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
-import java.time.LocalTime
 
-private fun infiniteFlow() = flow {
-    while (true) {
-        emit(LocalTime.now())
-        delay(1000)
-    }
+private fun infiniteFlow(interval: Long = 1000) = generateSequence(0L, Long::inc).asFlow().onEach {
+    delay(interval)
 }
 
 private suspend fun main() = coroutineScope {

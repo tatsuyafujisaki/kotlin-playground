5f5f92751437ed6cea30db9247795162a1a1ab8a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-12 10:40:33 +0900

    Delete InfiniteFlowExample2

--- src/main/kotlin/flow/InfiniteFlowExample.kt
+++ src/main/kotlin/flow/InfiniteFlowExample.kt
@@ -2,19 +2,29 @@ package flow
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
 import java.time.LocalTime
 
-private fun infiniteFlow() = flow {
+private fun infiniteFlow1() = flow {
     while (true) {
         emit(LocalTime.now())
         delay(1000)
     }
 }
 
+private fun infiniteFlow2(interval: Long = 1000) = generateSequence(0L, Long::inc).asFlow().onEach {
+    delay(interval)
+}
+
 private suspend fun main() = coroutineScope {
-    infiniteFlow().take(5).collect {
+    infiniteFlow1().take(3).collect {
+        println(it)
+    }
+
+    infiniteFlow2().take(3).collect {
         println(it)
     }
 }

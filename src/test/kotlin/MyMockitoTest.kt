07c8073680ea506f57187cd8caa5e70e8931c7e2
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-14 23:37:52 +0900

    Update MyMockitoTest.kt

--- src/test/kotlin/MyMockitoTest.kt
+++ src/test/kotlin/MyMockitoTest.kt
@@ -41,22 +41,6 @@ class MyMockitoTest {
         verify(mock, never()).f2()
         verify(mock).uppercase(uppercase)
     }
-
-    @Test
-    fun test3() {
-        val mock = mock<FinalMockMe>()
-
-        val clazz = MyClass3(mock)
-        clazz.setP1(s)
-        clazz.f1wrapper()
-        clazz.myUpperCaseWrapper(original)
-
-        verify(mock).p1 = s
-        verify(mock, never()).p2 = anyString()
-        verify(mock).f1()
-        verify(mock, never()).f2()
-        verify(mock).uppercase(uppercase)
-    }
 }
 
 interface MockMeInterface {
@@ -70,20 +54,10 @@ interface MockMeInterface {
 
 open class OpenMockMe {
     lateinit var p1: String
-    lateinit var p2: String
 
     open fun f1() {}
     open fun f2() {}
     open fun uppercase(s: String) {}
-}
-
-class FinalMockMe {
-    lateinit var p1: String
-    lateinit var p2: String
-
-    fun f1() {}
-    fun f2() {}
-    fun uppercase(s: String) {}
 }
 
 class MyClass1(private val clazz: MockMeInterface) {
@@ -101,20 +75,6 @@ class MyClass1(private val clazz: MockMeInterface) {
 }
 
 class MyClass2(private val clazz: OpenMockMe) {
-    fun setP1(s: String) {
-        clazz.p1 = s
-    }
-
-    fun f1wrapper() {
-        clazz.f1()
-    }
-
-    fun myUpperCaseWrapper(s: String) {
-        clazz.uppercase(s.uppercase())
-    }
-}
-
-class MyClass3(private val clazz: FinalMockMe) {
     fun setP1(s: String) {
         clazz.p1 = s
     }

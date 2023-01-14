fdc3fd1befa00ca20711251f3830ba7d71e01414
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-14 18:51:55 +0900

    Delete TimeUtil.kt

--- src/main/kotlin/util/TimeUtil.kt
+++
@@ -1,8 +0,0 @@
-package util
-
-import kotlin.time.Duration.Companion.milliseconds
-
-object TimeUtil {
-    val java.time.Duration.kotlinDuration: kotlin.time.Duration
-        get() = toMillis().milliseconds
-}

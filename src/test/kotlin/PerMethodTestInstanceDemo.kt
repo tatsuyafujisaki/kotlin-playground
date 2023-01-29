be9c6ca2800ea6d254955b08ea3e06d1df1ff8b9
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-29 09:50:53 +0900

    Create PerMethodTestInstanceDemo.kt

---
+++ src/test/kotlin/PerMethodTestInstanceDemo.kt
@@ -0,0 +1,20 @@
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import org.junit.jupiter.api.Test
+
+class PerMethodTestInstanceDemo {
+    private var b = false
+
+    @Test
+    fun assertTrueTest1() {
+        b = true
+        assertTrue(b)
+    }
+
+    @Test
+    fun assertFalseTest2() {
+        // Although b is set to true in assertTrueTest1(),
+        // b is false here because a new instance of the test class is created before each test method is executed.
+        assertFalse(b)
+    }
+}

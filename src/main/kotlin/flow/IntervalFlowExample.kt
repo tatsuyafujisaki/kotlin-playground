0dc8a803571485bd0024fbb2d665c67b99bd4038
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-30 20:18:43 +0900

    Update IntervalFlowExample.kt

--- src/main/kotlin/flow/IntervalFlowExample.kt
+++ src/main/kotlin/flow/IntervalFlowExample.kt
@@ -1,15 +1,15 @@
 package flow
 
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.take
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.seconds
 
 /**
  * Usage: interval(1.seconds)
@@ -26,28 +26,29 @@ private fun countUp(period: Duration, initialDelay: Duration = Duration.ZERO) =
     generateSequence(0L, Long::inc).asFlow().onStart { delay(initialDelay) }.onEach { delay(period) }
 
 private fun countDown(
-    millisInFuture: Long, countDownInterval: Long, initialDelay: Duration = Duration.ZERO, onFinish: () -> Unit
+    period: Long, interval: Long, initialDelay: Duration = Duration.ZERO
 ) = flow {
     delay(initialDelay)
-    for (t in millisInFuture downTo 0 step countDownInterval) {
+    for (t in period downTo 0 step interval) {
         emit(t)
-        delay(countDownInterval)
+        delay(interval)
     }
-    onFinish()
 }
 
 private suspend fun main() = coroutineScope {
     interval(1.seconds).take(3).collect {
-        println("collect $it")
+        println("collect: $it")
     }
 
     countUp(1.seconds).take(3).collect {
-        println("collect $it")
+        println("collect: $it")
     }
 
     countDown(
-        millisInFuture = 10.seconds.inWholeMilliseconds, countDownInterval = 500.milliseconds.inWholeMilliseconds
-    ) { println("onFinish()") }.collect {
-        println("collect: $it")
-    }
+        period = 3_000,
+        interval = 1_000
+    ).onCompletion { println("onCompletion") }
+        .collect {
+            println("collect: $it")
+        }
 }

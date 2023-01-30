006a989343de43b25a814ed64dbbcb3b98e63084
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-30 14:37:18 +0900

    Update IntervalFlowExample.kt

--- src/main/kotlin/flow/IntervalFlowExample.kt
+++ src/main/kotlin/flow/IntervalFlowExample.kt
@@ -1,7 +1,5 @@
 package flow
 
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
@@ -9,6 +7,8 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.take
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Usage: interval(1.seconds)
@@ -21,10 +21,19 @@ private fun interval(period: Duration, initialDelay: Duration = Duration.ZERO) =
     }
 }
 
-private fun counter(period: Duration, initialDelay: Duration = Duration.ZERO) = generateSequence(0L, Long::inc)
-    .asFlow()
-    .onStart { delay(initialDelay) }
-    .onEach { delay(period) }
+private fun interval2(
+    period: Duration, interval: Duration, initialDelay: Duration = Duration.ZERO, onFinish: () -> Unit
+) = flow {
+    delay(initialDelay)
+    for (t in period.inWholeMilliseconds downTo 0 step interval.inWholeMilliseconds) {
+        emit(Unit)
+        delay(period)
+    }
+    onFinish()
+}
+
+private fun counter(period: Duration, initialDelay: Duration = Duration.ZERO) =
+    generateSequence(0L, Long::inc).asFlow().onStart { delay(initialDelay) }.onEach { delay(period) }
 
 private suspend fun main() = coroutineScope {
     interval(1.seconds).take(3).collect {

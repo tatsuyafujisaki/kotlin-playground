41e4108d707ad1e1c32790ff88a80c70310afad9
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-02-25 16:38:26 +0900

    Update VirtualTimeTest.kt

--- src/test/kotlin/coroutines/VirtualTimeTest.kt
+++ src/test/kotlin/coroutines/VirtualTimeTest.kt
@@ -2,14 +2,21 @@ package coroutines
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
+import kotlin.time.measureTime
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 
-@OptIn(ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class, ExperimentalTime::class)
 class VirtualTimeTest {
     @Test
     fun coroutineTest() = runTest {
@@ -24,5 +31,22 @@ class VirtualTimeTest {
         assertEquals("b", x)
         advanceTimeBy(1)
         assertEquals("c", x)
+    }
+
+    /**
+     * [Delay-skipping](https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-test/kotlinx.coroutines.test/run-test.html)
+     */
+    @Test
+    fun measureTimeTest() = runTest {
+        val elapsed = TimeSource.Monotonic.measureTime {
+            val deferred = async {
+                delay(1.minutes) // skipped
+                withContext(Dispatchers.Default) {
+                    delay(5.seconds) // not skipped because Dispatchers.Default does not know about TestCoroutineScheduler.
+                }
+            }
+            deferred.await()
+        }
+        println(elapsed) // about five seconds
     }
 }

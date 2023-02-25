edceb1bfa14e262ebb889555254b3c92fa625d1d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-02-25 16:30:21 +0900

    Update TurbineTest.kt

--- src/test/kotlin/coroutines/TurbineTest.kt
+++ src/test/kotlin/coroutines/TurbineTest.kt
@@ -3,7 +3,10 @@ package coroutines
 import app.cash.turbine.test
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 
@@ -12,6 +15,19 @@ class TurbineTest {
     @Test
     fun test() = runTest {
         flowOf("a", "b").test {
+            assertEquals("a", awaitItem())
+            assertEquals("b", awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun test2() = runTest {
+        flow {
+            emit("a")
+            delay(10.seconds)
+            emit("b")
+        }.test {
             assertEquals("a", awaitItem())
             assertEquals("b", awaitItem())
             awaitComplete()

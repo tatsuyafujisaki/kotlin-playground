2a7dd7eaa9ff45c3f04f0793df0bcddc64b359ae
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-05-02 10:42:41 +0900

    Rename SafeStack to JavaStack

---
+++ src/main/kotlin/data/JavaStack.kt
@@ -0,0 +1,24 @@
+package data
+
+import java.util.Stack
+
+private class JavaStack<T> {
+    private val stack = Stack<T>()
+
+    fun push(item: T) = stack.push(item)
+    fun peek() = stack.peek() // throws EmptyStackException if the stack is empty.
+    fun pop() = stack.pop()
+    fun peekOrNull() = stack.lastOrNull()
+    fun popOrNull() = if (stack.empty()) null else stack.pop() // avoids an EmptyStackException.
+}
+
+private fun main() {
+    val stack = JavaStack<String>()
+    stack.push("a")
+    stack.push("b")
+
+    println(stack.pop()) // b
+    println(stack.pop()) // a
+    println(stack.peekOrNull()) // null
+    println(stack.popOrNull()) // null
+}

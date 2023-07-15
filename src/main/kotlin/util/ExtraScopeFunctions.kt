ddc416cd8038585335c02ab885f3731a0cb8bad5
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-07-15 20:13:54 +0900

    Delete ExtraScopeFunctions.kt

--- src/main/kotlin/util/ExtraScopeFunctions.kt
+++
@@ -1,4 +0,0 @@
-package util
-
-fun <T> T.applyIf(condition: Boolean, block: T.() -> T) = if (condition) block() else this
-fun <T> T.letIf(condition: Boolean, block: (T) -> T) = if (condition) block(this) else this

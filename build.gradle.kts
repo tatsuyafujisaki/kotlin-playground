03faf9aec656a16045952a2f868ffb6758e088c4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-24 13:32:46 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,6 @@ repositories {
 }
 
 dependencies {
-    /*
-     * Since Kotlin 1.4, the standard library has been added automatically.
-     * https://kotlinlang.org/docs/reference/whatsnew14.html#dependency-on-the-standard-library-added-by-default
-     */
-
-    /*
-     * Since Kotlin 1.1.2, the versions of artifacts of the group "org.jetbrains.kotlin" have been optional and inferred from the applied plugin.
-     * (No documentation about it is found.)
-     */
-
     implementation(libs.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.moshi.kotlin)

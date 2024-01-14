8e41ee891aa39c8627ca658af9f6c7f2868fe7f7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-01-14 14:38:34 +0900

    Update UiState.kt

--- src/main/kotlin/data/UiState.kt
+++ src/main/kotlin/data/UiState.kt
@@ -4,7 +4,7 @@ package data
  * [c.f.](https://developer.android.com/codelabs/basic-android-kotlin-compose-getting-data-internet#6)
  */
 sealed interface UiState {
-    object Loading : UiState
+    data object Loading : UiState
     data class Success<T>(val data: T) : UiState
     data class Failure(val throwable: Throwable) : UiState
 }

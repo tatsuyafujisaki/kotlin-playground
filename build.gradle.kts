280aa4ea1e0f46ea0999510c6065902bce68c96c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-02-26 13:37:36 +0900

    Add org.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(libs.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.moshi.kotlin)
+    implementation(libs.obsolete.json)
     implementation(libs.rxjava)
     testImplementation(kotlin("test"))
     testImplementation(libs.coroutines.test)

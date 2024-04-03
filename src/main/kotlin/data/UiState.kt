9bf038dda7fc36eb9316a4f9175aff62e1884d74
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-03 15:52:12 +0900

    Update UiState.kt

--- src/main/kotlin/data/UiState.kt
+++ src/main/kotlin/data/UiState.kt
@@ -1,10 +1,25 @@
 package data
 
 /**
- * [c.f.](https://developer.android.com/codelabs/basic-android-kotlin-compose-getting-data-internet#6)
+ * https://kotlinlang.org/docs/sealed-classes.html#state-management-in-ui-applications
+ * https://developer.android.com/codelabs/basic-android-kotlin-compose-getting-data-internet#6
  */
-sealed interface UiState {
-    data object Loading : UiState
-    data class Success<T>(val data: T) : UiState
-    data class Failure(val throwable: Throwable) : UiState
+sealed class UiState<out T> {
+    data object Loading : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Failure(val throwable: Throwable) : UiState<Nothing>()
+}
+
+private fun printState(state: UiState<String>) {
+    when (state) {
+        UiState.Loading -> println("Loading")
+        is UiState.Success -> println("Success: ${state.data}")
+        is UiState.Failure -> println("Failure: ${state.throwable}")
+    }
+}
+
+private fun main() {
+    printState(UiState.Loading)
+    printState(UiState.Success("Success!"))
+    printState(UiState.Failure(Throwable("Failure!")))
 }

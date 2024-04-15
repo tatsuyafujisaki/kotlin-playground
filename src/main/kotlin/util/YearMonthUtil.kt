8af865091c429124ec7397cd5f90f7216ae490a1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-15 23:20:52 +0900

    Update YearMonthUtil.kt

--- src/main/kotlin/util/YearMonthUtil.kt
+++ src/main/kotlin/util/YearMonthUtil.kt
@@ -9,6 +9,21 @@ object YearMonthUtil {
         YearMonth.parse(text, DateTimeFormatter.ofPattern(pattern))
     }.getOrNull()
 
+    /**
+     * @param n 0 means current month, 1 means last month, 2 means the month before last month, and so on.
+     */
+    fun getPastMonths(n: Int = 0): List<YearMonth> {
+        val now = YearMonth.now()
+        return List(n + 1) {
+            now.minusMonths(it.toLong())
+        }
+    }
+
     fun convertToDateAtDayOne(yearMonth: YearMonth = YearMonth.now()): LocalDate = yearMonth.atDay(1)
     fun convertToDateAtEndOfMonth(yearMonth: YearMonth = YearMonth.now()): LocalDate = yearMonth.atEndOfMonth()
+}
+
+private fun main() {
+    val pastMonths = YearMonthUtil.getPastMonths(0)
+    println(pastMonths)
 }

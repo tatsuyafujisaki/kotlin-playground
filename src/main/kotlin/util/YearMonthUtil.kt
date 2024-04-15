c3897a788b10681dd7db1330d4ee0759a68a038f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-15 23:12:00 +0900

    Update YearMonthUtil.kt

--- src/main/kotlin/util/YearMonthUtil.kt
+++ src/main/kotlin/util/YearMonthUtil.kt
@@ -2,8 +2,13 @@ package util
 
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 object YearMonthUtil {
+    fun parseStringOrNull(text: String, pattern: String) = runCatching {
+        YearMonth.parse(text, DateTimeFormatter.ofPattern(pattern))
+    }.getOrNull()
+
     fun convertToDateAtDayOne(yearMonth: YearMonth = YearMonth.now()): LocalDate = yearMonth.atDay(1)
     fun convertToDateAtEndOfMonth(yearMonth: YearMonth = YearMonth.now()): LocalDate = yearMonth.atEndOfMonth()
 }

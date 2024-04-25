5601535b2bd9be090049ecc1458fec0f557542b4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-25 22:26:44 +0900

    Update LocalDateTimeUtil.kt

--- src/main/kotlin/util/datetime/LocalDateTimeUtil.kt
+++ src/main/kotlin/util/datetime/LocalDateTimeUtil.kt
@@ -4,12 +4,15 @@ import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.util.Date
 import kotlin.time.toKotlinDuration
 
 object LocalDateTimeUtil {
     fun create(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int): LocalDateTime = LocalDateTime.of(year, month, day, hour, minute, second)
+    fun create(epochSecond: Long) = LocalDateTime.ofEpochSecond(epochSecond, 0, ZoneOffset.UTC)
     fun create(date: Date): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(date.time), ZoneId.systemDefault())
+    fun convertToEpochSecond(localDateTime: LocalDateTime) = localDateTime.toEpochSecond(ZoneOffset.UTC)
     fun isLessThanHoursOld(pastDateTime: LocalDateTime = LocalDateTime.now(), hours: Long) = pastDateTime.isAfter(LocalDateTime.now().minusHours(hours))
     fun convertObsoleteJavaUtilDateToLocalDateTime(date: Date = Date()): LocalDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
     fun formatJapanesePassedDateTime(dateTime1: LocalDateTime, date2: LocalDateTime): String {
@@ -26,5 +29,6 @@ object LocalDateTimeUtil {
 
 private fun main() {
     println(LocalDateTimeUtil.create(2025, 1, 2, 3, 4, 5))
+    println(LocalDateTimeUtil.create(LocalDateTimeUtil.convertToEpochSecond(LocalDateTime.now())))
     println(LocalDateTimeUtil.create(Date()))
 }

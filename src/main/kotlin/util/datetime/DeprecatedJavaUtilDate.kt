765351860107e405b9cc6d7fce98d4126c172e5a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-25 21:21:30 +0900

    Update Date-related utils

--- src/main/kotlin/util/datetime/DeprecatedJavaUtilDate.kt
+++ src/main/kotlin/util/datetime/DeprecatedJavaUtilDate.kt
@@ -7,17 +7,14 @@ import java.util.Date
 import kotlin.time.Duration.Companion.minutes
 
 object DeprecatedJavaUtilDate {
-    fun create(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int): Date =
-            Calendar.getInstance().apply {
-                set(year, month - 1, day, hour, minute, second)
-            }.time
+    fun create(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int): Date = Calendar.getInstance().apply {
+        set(year, month - 1, day, hour, minute, second)
+    }.time
 
     fun create(localDateTime: LocalDateTime): Date = Date.from(localDateTime.atZone(ZoneOffset.systemDefault()).toInstant())
-    private fun minutesPastFromNow(minutes: Long = 0) = Date(System.currentTimeMillis() - minutes.minutes.inWholeMilliseconds)
-    fun isWithinMinutesPastOrFuture(date: Date = Date(), minutes: Long = 0) = date.after(minutesPastFromNow(minutes))
+    fun isLessThanMinutesOld(date: Date, minutes: Long) = date.after(Date(System.currentTimeMillis() - minutes.minutes.inWholeMilliseconds))
 }
 
 private fun main() {
     println(DeprecatedJavaUtilDate.create(2025, 1, 2, 3, 4, 5))
-    println(DeprecatedJavaUtilDate.create(LocalDateTime.now()))
 }
--- src/main/kotlin/util/datetime/LocalDateTimeUtil.kt
+++ src/main/kotlin/util/datetime/LocalDateTimeUtil.kt
@@ -8,9 +8,9 @@ import java.util.Date
 import kotlin.time.toKotlinDuration
 
 object LocalDateTimeUtil {
-    fun create(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) = LocalDateTime.of(year, month, day, hour, minute, second)
+    fun create(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int): LocalDateTime = LocalDateTime.of(year, month, day, hour, minute, second)
     fun create(date: Date): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(date.time), ZoneId.systemDefault())
-    fun isWithinHoursPastOrFuture(pastDateTime: LocalDateTime = LocalDateTime.now(), hours: Long = 0): Boolean = pastDateTime.isAfter(LocalDateTime.now().minusHours(hours))
+    fun isLessThanMinutesOld(pastDateTime: LocalDateTime = LocalDateTime.now(), hours: Long = 0) = pastDateTime.isAfter(LocalDateTime.now().minusHours(hours))
     fun convertObsoleteJavaUtilDateToLocalDateTime(date: Date = Date()): LocalDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
     fun formatJapanesePassedDateTime(dateTime1: LocalDateTime, date2: LocalDateTime): String {
         val duration = Duration.between(dateTime1, date2).toKotlinDuration()

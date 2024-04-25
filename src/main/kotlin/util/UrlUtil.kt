5adad715b7ea3012be6afa438f242a685d90eb32
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-04-25 22:04:39 +0900

    Update UrlUtil.kt

--- src/main/kotlin/util/UrlUtil.kt
+++ src/main/kotlin/util/UrlUtil.kt
@@ -1,16 +1,20 @@
 package util
 
+import java.net.URLEncoder
+
 object UrlUtil {
+    fun encode(url: String) = URLEncoder.encode(url, Charsets.UTF_8.name())
+
     /**
      * [MatchResult.groupValues] returns a list of two elements.
      * The first element is the entire match including a trailing double quotation mark we don't want.
      * The second element is the first match, which is what we want.
      */
-    private fun extractDoubleQuotedUrl(s: String) = Regex("(https?://.+?)\"").find(s)?.groupValues?.last()
+    fun extractDoubleQuotedUrl(s: String) = Regex("(https?://.+?)\"").find(s)?.groupValues?.last()
+}
 
-    fun example() {
-        val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
-        val url = extractDoubleQuotedUrl(html) // https://example.com
-        println(url)
-    }
+private fun main() {
+    val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
+    val url = UrlUtil.extractDoubleQuotedUrl(html) // https://example.com
+    println(url)
 }

9e4b7dd3e103072c6ce08b8aabc0aeb8539ded57
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-08 22:07:48 +0900

    Delete LocalTimeSerializer

--- src/main/kotlin/json/kotlinserialization/datetime/DateTimeSerializationExample.kt
+++ src/main/kotlin/json/kotlinserialization/datetime/DateTimeSerializationExample.kt
@@ -4,12 +4,10 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 
 @Serializable
 private data class MySerializable(
         @Serializable(with = LocalDateSerializer::class) val localDate: LocalDate,
-        @Serializable(with = LocalTimeSerializer::class) val localTime: LocalTime,
         @Serializable(with = LocalDateTimeSerializer::class) val localDateTime: LocalDateTime,
 )
 
@@ -17,9 +15,8 @@ private fun main() {
     val json = """
         {
             "localDate": "2025-01-02",
-            "localTime": "12:34:56",
             "localDateTime": "2025-01-02 12:34:56"
-        }       
+        }
     """.trimIndent()
 
     val serializable = Json.decodeFromString<MySerializable>(string = json)

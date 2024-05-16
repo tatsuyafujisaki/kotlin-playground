01eefd37d499c083820ee28422bc67a736cfb152
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-16 13:23:44 +0900

    Update CollectionUtil.kt

--- src/main/kotlin/util/CollectionUtil.kt
+++ src/main/kotlin/util/CollectionUtil.kt
@@ -1,6 +1,8 @@
 package util
 
 object CollectionUtil {
+    fun <T> Collection<T>?.isNeitherNullNorEmpty() = !isNullOrEmpty()
+
     fun <T> Collection<T>.permute(): List<List<T>> {
         fun <T> Collection<T>.permute(result: List<T> = emptyList()): List<List<T>> = if (isEmpty()) {
             listOf(result)
@@ -13,4 +15,14 @@ object CollectionUtil {
     }
 
     fun <T> Collection<T>.permuteWithoutDuplicates() = permute().distinct()
+}
+
+private fun main() {
+    val xs: List<String>? = listOf("a")
+    val ys: List<String>? = emptyList()
+    val zs: List<String>? = null
+
+    println(xs.isNeitherNullNorEmpty()) // true
+    println(ys.isNeitherNullNorEmpty()) // false
+    println(zs.isNeitherNullNorEmpty()) // false
 }

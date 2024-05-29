1e0fb369909b56c0083f80730c01de02a8feecde
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-05-29 16:28:06 +0900

    Update UrlUtil.kt

--- src/main/kotlin/util/UrlUtil.kt
+++ src/main/kotlin/util/UrlUtil.kt
@@ -3,18 +3,12 @@ package util
 import java.net.URLEncoder
 
 object UrlUtil {
-    fun encode(url: String) = URLEncoder.encode(url, Charsets.UTF_8.name())
-
     /**
-     * [MatchResult.groupValues] returns a list of two elements.
-     * The first element is the entire match including a trailing double quotation mark we don't want.
-     * The second element is the first match, which is what we want.
+     * Instead, use Uri.encode in Android.
+     * https://developer.android.com/reference/kotlin/android/net/Uri#encode
      */
-    fun extractDoubleQuotedUrl(s: String) = Regex("(https?://.+?)\"").find(s)?.groupValues?.last()
-}
+    private fun encode(url: String) = URLEncoder.encode(url, Charsets.UTF_8.name())
 
-private fun main() {
-    val html = "<a href=\"https://example.com\"><img src=\"sample.png\"></a>"
-    val url = UrlUtil.extractDoubleQuotedUrl(html) // https://example.com
-    println(url)
+    fun getNthLevelDomain(domain: String, level: Int) =
+            domain.split(".").takeLast(level).joinToString(".")
 }

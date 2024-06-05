2c375498c3de1ebb2760c2e5c3fa474e9a8a26f2
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-05 15:17:34 +0900

    Update DeprecationExample.kt

--- src/main/kotlin/example/DeprecationExample.kt
+++ src/main/kotlin/example/DeprecationExample.kt
@@ -2,16 +2,20 @@ package example
 
 object DeprecationExample {
     @Deprecated(
-            message = "Use myNewFunction(String) instead.",
-            replaceWith = ReplaceWith(
-                    expression = "myNewFunction(String)",
-                    imports = ["example.DeprecationExample"]
-            ),
+        message = "Use myNewFunction(String) instead.",
+        // The deprecated declaration is replaced by the following expression when saving a file
+        // if Settings > "Actions on Save" > "Run code cleanup" is enabled.
+        replaceWith = ReplaceWith(expression = "myNewFunction(String)"),
     )
-    fun myOldFunction() {
+    fun myOldFunction(s: String) {
+        println(s)
     }
 
     fun myNewFunction(s: String) {
         println(s)
     }
 }
+
+// private fun main() {
+//     myOldFunction("Hello")
+// }

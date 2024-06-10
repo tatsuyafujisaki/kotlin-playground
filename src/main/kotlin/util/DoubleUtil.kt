7234b40cb528605ed190e2c57507386f4a14dd03
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-11 07:18:34 +0900

    Update DoubleUtil.kt

--- src/main/kotlin/util/DoubleUtil.kt
+++ src/main/kotlin/util/DoubleUtil.kt
@@ -1,10 +1,6 @@
 package util
 
-import java.math.BigInteger
-
 object DoubleUtil {
-    /** Redundant wrapper */
-    fun Double.roundFormat(decimals: Int) = "%.${decimals}f".format(this)
-
-    fun Double.toBigInteger(): BigInteger = toBigDecimal().toBigInteger()
+    private fun getBigInteger(x: Double) = x.toBigDecimal().toBigInteger()
+    private fun roundFormat(x: Double, decimals: Int) = "%.${decimals}f".format(x)
 }

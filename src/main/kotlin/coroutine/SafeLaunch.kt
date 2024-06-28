879ae5458d118a817baeaca154a9e24a8c5b4a09
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-28 22:11:46 +0900

    Create SafeLaunch.kt

---
+++ src/main/kotlin/coroutine/SafeLaunch.kt
@@ -0,0 +1,36 @@
+package coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
+
+fun CoroutineScope.safeLaunch(
+    handleException: CoroutineScope.(Exception) -> Unit = {},
+    handleCancellationException: CoroutineScope.(CancellationException) -> Unit = handleException,
+    tryBlock: suspend CoroutineScope.() -> Unit,
+): Job = launch {
+    try {
+        tryBlock()
+    } catch (e: CancellationException) {
+        handleCancellationException(e)
+        throw e
+    } catch (e: Exception) {
+        handleException(e)
+    }
+}
+
+private suspend fun main() = coroutineScope {
+    val job = safeLaunch(
+        handleException = { e -> println("👀$e") },
+    ) {
+        println("try started!")
+        delay(timeMillis = 10_000)
+        println("try is ending!")
+    }
+    job.cancelAndJoin()
+    println("job: $job")
+}

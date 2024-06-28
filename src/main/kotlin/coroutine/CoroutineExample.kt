006906ea569d662c7967ebe1821e3bd60823c644
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-28 18:16:35 +0900

    Update CoroutineExample.kt

--- src/main/kotlin/coroutine/CoroutineExample.kt
+++ src/main/kotlin/coroutine/CoroutineExample.kt
@@ -1,5 +1,6 @@
 package coroutine
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
@@ -11,6 +12,8 @@ private suspend fun main() = coroutineScope {
             println("The job started!")
             delay(timeMillis = 10_000)
             println("The job ended!")
+        } catch (e: CancellationException) {
+            throw e
         } catch (e: Exception) {
             println(e)
         }

03f77df4edbc9da3534dd454c02e5f81df573ea7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-28 16:57:38 +0900

    Create CoroutineExample.kt

---
+++ src/main/kotlin/coroutine/CoroutineExample.kt
@@ -0,0 +1,21 @@
+package coroutine
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private suspend fun main() = coroutineScope {
+    val job = launch {
+        try {
+            println("The job started!")
+            delay(timeMillis = 10_000)
+            println("The job ended!")
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+    delay(timeMillis = 1) // waits a little bit or the job will be canceled even before it starts.
+    job.cancelAndJoin()
+    println("Done!")
+}

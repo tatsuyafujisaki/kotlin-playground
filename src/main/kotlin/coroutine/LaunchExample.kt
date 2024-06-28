13f2b9cab7155bb51eb52ab74788bb7441a9723c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-28 19:17:33 +0900

    Create LaunchExample.kt

---
+++ src/main/kotlin/coroutine/LaunchExample.kt
@@ -0,0 +1,24 @@
+package coroutine
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private suspend fun main() = coroutineScope {
+    val job = launch {
+        try {
+            println("try started!")
+            delay(timeMillis = 10_000)
+            println("try is ending!")
+        } catch (e: CancellationException) {
+            println("👀$e")
+            throw e
+        } catch (e: Exception) {
+            println("👀$e")
+        }
+    }
+    job.cancelAndJoin()
+    println("job: $job")
+}

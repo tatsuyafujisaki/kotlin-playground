60ba0f773abaaa3693175a32f63799d5e0583325
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-29 14:01:06 +0900

    Update coRunCatching.kt

--- src/main/kotlin/coroutine/coRunCatching.kt
+++ src/main/kotlin/coroutine/coRunCatching.kt
@@ -1,11 +1,9 @@
 package coroutine
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.launch
 
 /**
  * https://detekt.dev/docs/rules/coroutines/#suspendfunswallowedcancellation
@@ -14,29 +12,73 @@ inline fun <T> CoroutineScope.coRunCatching(block: CoroutineScope.() -> T): Resu
     return try {
         Result.success(value = block())
     } catch (exception: Throwable) {
-        ensureActive()
+        //ensureActive()
         Result.failure(exception = exception)
     }
 }
 
 /**
- * Result:
- * Done!
+ * This example demonstrates the behavior described below.
+ * > if the coroutine in which await was called got cancelled,
+ * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines/-deferred/await.html
  *
- * If `ensureActive()` inside coRunCatching is uncommented, the output will be:
- * onFailure: kotlinx.coroutines.JobCancellationException: StandaloneCoroutine was cancelled
+ * Result:
+ * try started!
+ * async started!
+ * onFailure: kotlinx.coroutines.JobCancellationException: DeferredCoroutine was cancelled; job=DeferredCoroutine{Cancelled}@64db3069
+ * Done!
  */
-private suspend fun main(): Unit = coroutineScope {
-    val job = launch {
-        coRunCatching {
+private suspend fun doExample1() = coroutineScope {
+    coRunCatching {
+        println("try started!")
+        val deferred = async {
+            println("async started!")
             delay(timeMillis = 100) // waits to avoid completing before being cancelled.
-            println("I am not printed because the job is canceled before me.")
-        }.onSuccess {
-            println("onSuccess: $it")
-        }.onFailure {
-            println("onFailure: $it")
+            println("async ended!")
         }
+        deferred.cancel()
+        deferred.await() // throws CancellationException.
+        println("try ended!")
+    }.onSuccess {
+        println("onSuccess: $it")
+    }.onFailure {
+        // The following is executed because `ensureActive()` inside safeLaunch does not throw CancellationException.
+        println("onFailure: $it")
     }
-    job.cancelAndJoin()
     println("Done!")
+}
+
+/**
+ * This example demonstrates the behavior described below.
+ * > or if the Deferred itself got completed with a CancellationException.
+ * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines/-deferred/await.html
+ *
+ * Result:
+ * try started!
+ * async started!
+ * onFailure: kotlinx.coroutines.JobCancellationException: DeferredCoroutine was cancelled; job=DeferredCoroutine{Cancelled}@1c5f44ad
+ * Done!
+ */
+private suspend fun doExample2() = coroutineScope {
+    coRunCatching {
+        println("try started!")
+        val deferred = async {
+            println("async started!")
+            delay(timeMillis = 100) // waits to avoid completing before being cancelled.
+            println("async ended!")
+        }
+        deferred.cancel()
+        deferred.await() // throws CancellationException.
+        println("try ended!")
+    }.onSuccess {
+        println("onSuccess: $it")
+    }.onFailure {
+        // The following is executed because `ensureActive()` inside safeLaunch does not throw CancellationException.
+        println("onFailure: $it")
+    }
+    println("Done!")
+}
+
+private suspend fun main() = coroutineScope {
+    doExample2()
 }

a0d51230cccea6df0f48f43d268fe3e9555b73a5
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-06-29 09:03:58 +0900

    Update AsyncAwaitExample.kt

--- src/main/kotlin/coroutine/AsyncAwaitExample.kt
+++ src/main/kotlin/coroutine/AsyncAwaitExample.kt
@@ -1,25 +1,54 @@
 package coroutine
 
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 
-private suspend fun main() = coroutineScope {
+private suspend fun CoroutineScope.doExample1() {
     val deferred = async {
-        try {
-            println("try started!")
-            delay(timeMillis = 1_000)
-            println("try is ending!")
-        } catch (e: CancellationException) {
-            println("👀$e")
-            throw e
-        } catch (e: Exception) {
-            println("👀$e")
-        }
-        "🍎"
+        println("async started!")
+        // Delay this coroutine a little so that it does not complete before being cancelled.
+        delay(100)
+        println("async is ending!")
+        throw CancellationException()
+    }
+    try {
+        println("try started!")
+        deferred.await()
+        println("try is ending!")
+    } catch (e: CancellationException) {
+        println("catch started!")
+        currentCoroutineContext().ensureActive() // throws if the current coroutine was cancelled
+        println(e) // if this line executes, the exception is the result of `await` itself
+        println("catch is ending!")
+    }
+    println("Done!")
+}
+
+private suspend fun CoroutineScope.doExample2() {
+    val deferred = async {
+        println("async started!")
+        // Delay this coroutine a little so that it does not complete before being cancelled.
+        delay(100)
+        println("async is ending!")
     }
     deferred.cancel()
-    val result = deferred.await()
-    println("result: $result")
+    try {
+        deferred.await()
+        println("try is ending!")
+    } catch (e: CancellationException) {
+        println("catch started!")
+        currentCoroutineContext().ensureActive() // throws if the current coroutine was cancelled
+        println(e) // if this line executes, the exception is the result of `await` itself
+        println("catch is ending!")
+    }
+    println("Done!")
+}
+
+private suspend fun main() = coroutineScope {
+    doExample1()
 }

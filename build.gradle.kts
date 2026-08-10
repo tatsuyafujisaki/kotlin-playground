54efd81e97b89aac76c77333c84ee9e2b92d1248
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-08-11 00:19:18 +0900

    Add kotlinx-datetime dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(libs.coroutines.core)
     implementation(libs.gson)
     implementation(libs.json)
+    implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
     testImplementation(kotlin("test"))
     testImplementation(libs.coroutines.test)
